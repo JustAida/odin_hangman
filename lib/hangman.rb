59349db26cdddb92394bc367d2e6ab88c1f73ae8
JustAida <suradet40@gmail.com>
2022-03-27 12:38:13 +0700

    Change the remaining guesses to the size of the template

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,7 +2,7 @@ module Hangman
   class Game
     def initialize
       @template = template
-      @remaining_guesses = 6
+      @remaining_guesses = @template.size
       @current_hangman = ""
       @secret_word = secret_word
       @incorrect_letters = []
@@ -20,6 +20,7 @@ module Hangman
       secret_words.sample
     end
 
+    # Change the template to increase or decrease the remaining guesses.
     def template
       [
         " ____",
@@ -32,9 +33,15 @@ module Hangman
     end
 
     def display_hangman
-      puts @current_hangman = @remaining_guesses == 6 ? "" : @template[@remaining_guesses..-1]
+      puts @current_hangman = @remaining_guesses == @template.size ? "" : @template[@remaining_guesses..-1]
     end
 
+    # Display the guesses:
+    #   _ for each letter that hasn't been guessed yet.
+    #   Correct letters that have been chosen in the correct position.
+    #   Incorrect letters that have already been chosen.
+    #   For example: H _ N G _ _ N
+    #                Incorrect letters: O J Z
     def display_guess_info
       guess_info = @secret_word.chars.map do |char|
         @correct_letters.include?(char) ? char : "_"
@@ -54,12 +61,6 @@ module Hangman
   end
 end
 
-# Display the guesses: 
-#   _ for each letter that hasn't been guessed yet.
-#   Correct letters that have been chosen in the correct position.
-#   Incorrect letters that have already been chosen.
-#   For example: H _ N G _ _ N [O, J, Z]
-
 # Ask the player for the guess each turn and it should be case-insensitive.
 # Update the display according to the guess.
 # Player will lose if out of guesses.
@@ -68,4 +69,4 @@ end
 
 # Ask the player when the program first loads if the player want to load one of the saves.
 
-puts Hangman::Game.new.play
+Hangman::Game.new.play
